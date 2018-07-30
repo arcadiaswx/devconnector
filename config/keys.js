99d3dc8dc475a352c0c1f3096dbdd9a4fc1f8c5f
Craig Booker <info@itech.plus>
2018-07-30 16:16:27 -0500

    Modified keys to relect the local dev env, modified server to put me in line with the videos.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
+module.exports = {
+  mongoURI: "mongodb://craig:ctb282@ds151180.mlab.com:51180/devconnector"
+};
+/*
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
   module.exports = require('./keys_dev');
 }
+*/
